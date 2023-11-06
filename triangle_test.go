f9ff9cc830fa73e4eb6bf091d5ec53090fbee1f7
tirouzh <ctirouzh@gmail.com>
2023-11-07 02:43:14 +0330

    Refactor tringle test

--- triangle_test.go
+++ triangle_test.go
@@ -1,22 +1,22 @@
 package shapes_test
 
 import (
-	shapes2 "playground"
+	shapes "playground"
 	"testing"
 )
 
 func TestNewWingedTriangle(t *testing.T) {
-	wingedtri, err := shapes2.NewWingedTriangle([3]float64{3, 4, 5})
+	wingedTri, err := shapes.NewWingedTriangle([3]float64{3, 4, 5})
 	if err != nil {
 		t.Errorf("expected nil error,got %s", err.Error())
 	}
-	if wingedtri == nil {
-		t.Error("expected a newwingedtriangle,but got nil !!??")
+	if wingedTri == nil {
+		t.Error("expected a new winged triangle, but got nil")
 	}
-	if wingedtri.AB() != 3 {
-		if wingedtri.BC() != 4 {
-			if wingedtri.CA() != 5 {
-				t.Errorf("expected ab=3 bc=4 ca=5 , got ab=%.2f bc=%.2f ca=%.2f", wingedtri.AB(), wingedtri.BC(), wingedtri.CA())
+	if wingedTri.AB() != 3 {
+		if wingedTri.BC() != 4 {
+			if wingedTri.CA() != 5 {
+				t.Errorf("expected ab=3 bc=4 ca=5 , got ab=%.2f bc=%.2f ca=%.2f", wingedTri.AB(), wingedTri.BC(), wingedTri.CA())
 			}
 		}
 	}
@@ -24,20 +24,35 @@ func TestNewWingedTriangle(t *testing.T) {
 }
 
 func TestNewTriangleWithPoint(t *testing.T) {
-	points := [3]shapes2.Point{
+	points := [3]shapes.Point{
 		{1, 2, 3},
 		{4, 5, 8},
 		{3, 5, 9},
 	}
-	newTriangleWithPoints, err := shapes2.NewTriangleWithPoints(points)
-
+	newTriangleWithPoints, err := shapes.NewTriangleWithPoints(points)
 	if err != nil {
-		t.Errorf("expected nil error,got %s", err.Error())
+		t.Errorf("expected nil error, got %s", err.Error())
 	}
 	if newTriangleWithPoints == nil {
-		t.Error("expected a newtriangle,but got nil !!??")
+		t.Error("expected a new triangle, but got nil")
 	}
-	if !shapes2.IsTriangle(points) {
-		t.Error("expect,points are equal and can not make a triangle,but got a triangle")
+}
+
+func TestIsTriangle(t *testing.T) {
+	points1 := [3]shapes.Point{
+		{1, 2, 3},
+		{4, 5, 8},
+		{3, 5, 9},
+	}
+	if !shapes.IsTriangle(points1) {
+		t.Error("expect true, but got false")
+	}
+	points2 := [3]shapes.Point{
+		{1, 2, 0},
+		{2, 4, 0},
+		{3, 6, 0},
+	}
+	if shapes.IsTriangle(points2) {
+		t.Error("expect false, but got true")
 	}
 }
